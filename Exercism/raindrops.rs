96a9d7d839afb2a3d668fc88b3c5eec867c97760
a.samartino <a.samartino@protonmail.com>
2022-12-21 09:28:36 +0100

    create raindrops.rs

---
+++ Exercism/raindrops.rs
@@ -0,0 +1,38 @@
+// Instructions
+// Your task is to convert a number into a string that contains raindrop sounds corresponding to certain potential factors. 
+// A factor is a number that evenly divides into another number, leaving no remainder. The simplest way to test if a one number 
+// is a factor of another is to use the modulo operation.
+
+// The rules of raindrops are that if a given number:
+
+// has 3 as a factor, add 'Pling' to the result.
+// has 5 as a factor, add 'Plang' to the result.
+// has 7 as a factor, add 'Plong' to the result.
+// does not have any of 3, 5, or 7 as a factor, the result should be the digits of the number.
+// Examples
+// 28 has 7 as a factor, but not 3 or 5, so the result would be "Plong".
+// 30 has both 3 and 5 as factors, but not 7, so the result would be "PlingPlang".
+// 34 is not factored by 3, 5, or 7, so the result would be "34".
+
+
+pub fn raindrops(n: u32) -> String {
+    let mut result = String::new();
+    if n% 3 == 0{
+        result.push_str("Pling");
+    }
+    if n% 5 == 0{
+        result.push_str("Plang");
+    }
+    if n% 7 == 0{
+        result.push_str("Plong");
+    }
+    if result.is_empty(){
+        result = n.to_string();
+    }
+    result
+}
+
+
+fn main() {
+    println!("Hello, world!");
+}
