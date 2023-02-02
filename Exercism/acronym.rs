00a9aed1276d3b92f579b40fefa5cd14752878cb
a.samartino <a.samartino@protonmail.com>
2023-02-02 10:40:31 +0100

    create acronym

---
+++ Exercism/acronym.rs
@@ -0,0 +1,24 @@
+// Instructions
+// Convert a phrase to its acronym.
+
+// Techies love their TLA (Three Letter Acronyms)!
+
+// Help generate some jargon by writing a program that converts a long name like Portable Network Graphics to its acronym (PNG).
+
+pub fn abbreviate(phrase: &str) -> String {
+    let mut result = String::new();
+
+    // data cleaning
+    let phrase = phrase.replace(&['-', '_', ':'][..], " ");
+
+    for iter in phrase.split_whitespace() {
+        if iter.chars().all(|c| c.is_uppercase()) || iter.chars().all(|c| c.is_lowercase()) {
+            result.push(iter.chars().nth(0).unwrap().to_ascii_uppercase())
+        } else {
+            // complication due to the test "HyperText" -> have to split the string
+            let abc: String = iter.chars().filter(|c| c.is_uppercase()).collect();
+            result.push_str(&abc);
+        }
+    }
+    result
+}
