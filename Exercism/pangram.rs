24b03a01697c93bbbe73908b4ac0faed29d41860
a.samartino <a.samartino@protonmail.com>
2023-02-10 08:44:25 +0100

    create pangram.rs

---
+++ Exercism/pangram.rs
@@ -0,0 +1,23 @@
+// Instructions
+// Determine if a sentence is a pangram. A pangram (Greek: παν γράμμα, pan gramma, "every letter") is a sentence using every letter of the alphabet at least once. The best known English pangram is:
+
+// The quick brown fox jumps over the lazy dog.
+
+// The alphabet used consists of ASCII letters a to z, inclusive, and is case insensitive. Any characters which are not an ASCII letter should be ignored.
+
+// Determine whether a sentence is a pangram.
+
+use std::collections::HashMap;
+
+pub fn is_pangram(sentence: &str) -> bool {
+    let sentence = sentence.to_ascii_lowercase();
+    let mut map: HashMap<char, u32> = ('a'..='z').map(|x| (x, 0)).collect();
+
+    for char in sentence.chars(){
+        if char >= 'a' || char <= 'z'{
+            *map.entry(char).or_insert(0) += 1;
+        }
+    }
+    map.values().all(|&x| x > 0)
+    
+}
